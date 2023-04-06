bbc0614054f4644c48b045c7dc6591b6c7d60a7d
sunfish141 <108562688+sunfish141@users.noreply.github.com>
2023-04-06 14:45:50 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 //INITIALIZE VARIABLES
 let x = canvas.width / 2;
 let y = canvas.height / 2;
+let health = 10000000;
 const ballRadius = 10;
 const bulletWidth = 5;
 const bulletHeight = 20;
@@ -21,7 +22,15 @@ let enemies = [];
 let rectangleEnemyLength = 50;
 let rectangleEnemyWidth = 50;
 let rectangleEnemyCooldown = 0;
-let rectangledelay = 1000;
+let rectangledelay = 700;
+let circularEnemyRadius = 30;
+let difficultygap = 10000;
+let lastDifficultyScale = 0;
+let enemydifficultyScale = 0;
+let shooterWidth = 20;
+let shooterLength = 25;
+let enemyBulletdelay = 1500;
+let enemybulletradius = 9;
 
 //POPULATE ARRAYS
 for (i = 0; i < 20; i++) {
@@ -34,7 +43,43 @@ for (i = 0; i < 20; i++) {
 for (i = 0; i < 20; i++) {
   enemies[i] = [];
   for (j = 0; j < 5; j++) {
-    enemies[i][j] = { x: 0, y: 0, status: false, moving: false, hits: 0 };
+    let setrectangularWeakness = Math.floor(Math.random() * 3);
+    if (enemydifficultyScale > 2) {
+      for (c = 0; c < enemydifficultyScale; c++) {
+        setrectangularWeakness = Math.floor(Math.random() * 3);
+        if (setrectangularWeakness == 0) {
+          if (Math.floor(Math.random() * enemydifficultyScale) == 0) {
+            break;
+          } else {
+            continue;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+    let enemyType = 0;
+    for (a = 0; a < enemydifficultyScale; a++) {
+      enemyType = Math.floor(Math.random() * 5);
+      if (enemyType == 0) {
+        break;
+      }
+    }
+    let type = "rectangular";
+    if (enemyType == 0) {
+      type = "circular";
+    }
+    enemies[i][j] = {
+      x: 0,
+      y: 0,
+      status: false,
+      moving: false,
+      hits: setrectangularWeakness,
+      type: type,
+      armour: 0,
+      shooter: false,
+      shotCooldown: 0,
+    };
   }
 }
 
@@ -45,6 +90,12 @@ function drawPlayer() {
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function drawHealth() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText(`Health: ${health}`, canvas.width - 120, 20);
 }
 
 //Create and draw bullets
@@ -83,7 +134,53 @@ function setEnemies() {
           return;
         } else {
           enemies[i][j].status = true;
-          enemies[i][j].hits = 0;
+          let enemyType = 0;
+          let shooter = false;
+          for (b = 0; b < enemydifficultyScale; b++) {
+            enemyType = Math.floor(Math.random() * 5);
+            if (enemyType == 0) {
+              break;
+            }
+          }
+          if (
+            enemydifficultyScale == 5 &&
+            Math.floor(Math.random() * 10) == 0 &&
+            shooter != true
+          ) {
+            shooter = true;
+          }
+          enemies[i][j].type = "rectangular";
+          if (enemyType == 0) {
+            enemies[i][j].type = "circular";
+          } else if (shooter == true) {
+            enemies[i][j].shooter = true;
+          }
+          let rectangularWeakness = Math.floor(Math.random() * 3);
+          if (enemydifficultyScale > 2) {
+            for (c = 0; c < enemydifficultyScale; c++) {
+              rectangularWeakness = Math.floor(Math.random() * 3);
+              if (rectangularWeakness == 0) {
+                if (Math.floor(Math.random() * enemydifficultyScale) == 0)
+                  break;
+                else {
+                  continue;
+                }
+              } else {
+                break;
+              }
+            }
+          }
+          enemies[i][j].hits = rectangularWeakness;
+          if (enemydifficultyScale >= 4) {
+            let armourChance = Math.floor(Math.random() * 5);
+            if (armourChance == 0) {
+              enemies[i][j].armour = Math.floor(Math.random() * 2 + 1);
+            } else {
+              enemies[i][j].armour = 0;
+            }
+          } else {
+            enemies[i][j].armour = 0;
+          }
           rectangleEnemyCooldown = Date.now();
         }
       }
@@ -102,13 +199,24 @@ function createEnemies() {
         enemies[i][j].y = 0;
         enemies[i][j].moving = true;
       } else if (enemies[i][j].status == true && enemies[i][j].moving == true) {
-        enemies[i][j].y += 1;
+        if (enemies[i][j].type == "rectangular") {
+          enemies[i][j].y += 1;
+        } else if (enemies[i][j].type == "circular") {
+          enemies[i][j].y += 2;
+        } else if (enemies[i][j].type == "shooterbullet") {
+          enemies[i][j].y += 3;
+        }
       }
-      if (enemies[i][j].y == canvas.height) {
+      if (enemies[i][j].y >= canvas.height && enemies[i][j].status == true) {
         enemies[i][j].status = false;
         enemies[i][j].moving = false;
+        if (enemies[i][j].type == "shooterbullet") {
+          health = health;
+        } else {
+          health -= 50;
+        }
       }
-      if (enemies[i][j].status == true) {
+      if (enemies[i][j].status == true && enemies[i][j].type == "rectangular") {
         ctx.beginPath();
         ctx.rect(
           enemies[i][j].x,
@@ -123,13 +231,125 @@ function createEnemies() {
         } else if (enemies[i][j].hits == 2) {
           ctx.fillStyle = "#8b0000";
         }
+        if (enemies[i][j].armour == 1) {
+          ctx.fillStyle = "#000000";
+        } else if (enemies[i][j].armour == 2) {
+          ctx.fillStyle = "#9D00FF";
+        }
+        ctx.fill();
+        ctx.closePath();
+        if (enemies[i][j].shooter == true) {
+          ctx.beginPath();
+          ctx.rect(
+            enemies[i][j].x + rectangleEnemyWidth / 2 - shooterWidth / 2,
+            enemies[i][j].y + rectangleEnemyLength,
+            shooterWidth,
+            shooterLength
+          );
+          ctx.fillStyle = "#808080";
+          ctx.fill();
+          ctx.closePath();
+        }
+      } else if (
+        enemies[i][j].status == true &&
+        enemies[i][j].type == "circular"
+      ) {
+        ctx.beginPath();
+        ctx.arc(
+          enemies[i][j].x,
+          enemies[i][j].y,
+          circularEnemyRadius,
+          0,
+          Math.PI * 2
+        );
+        if (enemies[i][j].hits == 0) {
+          ctx.fillStyle = "#228B22";
+        } else if (enemies[i][j].hits == 1) {
+          ctx.fillStyle = "#FFFF00";
+        } else if (enemies[i][j].hits == 2) {
+          ctx.fillStyle = "#8b0000";
+        }
+        if (enemies[i][j].armour == 1) {
+          ctx.fillStyle = "#000000";
+        } else if (enemies[i][j].armour == 2) {
+          ctx.fillStyle = "#9D00FF";
+        }
+        ctx.fill();
+        ctx.closePath();
+        if (enemies[i][j].shooter == true) {
+          ctx.beginPath();
+          ctx.rect(
+            enemies[i][j].x - shooterWidth / 2,
+            enemies[i][j].y + circularEnemyRadius - 3,
+            shooterWidth,
+            shooterLength
+          );
+          ctx.fillStyle = "#808080";
+          ctx.fill();
+          ctx.closePath();
+        }
+      } else if (
+        enemies[i][j].status == true &&
+        enemies[i][j].type == "shooterbullet"
+      ) {
+        ctx.beginPath();
+        ctx.arc(
+          enemies[i][j].x,
+          enemies[i][j].y,
+          enemybulletradius,
+          0,
+          Math.PI * 2
+        );
+        ctx.fillStyle = "#8B4000";
+        ctx.fill();
+        ctx.closePath;
       }
-      ctx.fill();
-      ctx.closePath();
     }
   }
 }
 
+//ALLOW SHOOTER TYPE ENEMIES TO CREATE BULLETS
+function bulletEnemies() {
+  for (i = 0; i < 20; i++) {
+    for (j = 0; j < 5; j++) {
+      if (enemies[i][j].shooter == true && enemies[i][j].status == true) {
+        let add = 0;
+        let bulletfound = false;
+        let yoffset = 0;
+        if (enemies[i][j].shotCooldown >= Date.now() - enemyBulletdelay) {
+          return;
+        } else {
+          for (p = 0; p < 20; p++) {
+            for (q = 0; q < 5; q++) {
+              if (enemies[p][q].status == false) {
+                if (enemies[i][j].type == "rectangular") {
+                  add = 50;
+                  yoffset = rectangleEnemyWidth / 2;
+                } else if (enemies[i][j].type == "circular") {
+                  add = 30;
+                }
+                enemies[p][q].x = enemies[i][j].x + yoffset;
+                enemies[p][q].y = enemies[i][j].y + add + shooterLength;
+                enemies[p][q].status = true;
+                enemies[p][q].moving = true;
+                enemies[p][q].type = "shooterbullet";
+                enemies[p][q].hits = 2;
+                bulletfound = true;
+                break;
+              }
+            }
+            if (bulletfound == true) {
+              break;
+            }
+          }
+          enemies[i][j].shotCooldown = Date.now();
+        }
+      }
+    }
+  }
+}
+
+///DeTECT BULLET, ENEMY COLLISION
 function detectCollisions() {
   for (i = 0; i < 20; i++) {
     for (j = 0; j < 5; j++) {
@@ -141,45 +361,164 @@ function detectCollisions() {
             bullets[q][p].y > enemies[i][j].y &&
             bullets[q][p].y < enemies[i][j].y + rectangleEnemyLength &&
             bullets[q][p].shot == true &&
-            enemies[i][j].status == true
+            enemies[i][j].status == true &&
+            enemies[i][j].type == "rectangular"
           ) {
-            enemies[i][j].hits++;
+            if (enemies[i][j].armour > 0) {
+              enemies[i][j].armour--;
+            } else {
+              enemies[i][j].hits++;
+            }
             bullets[q][p].shot = false;
             bullets[q][p].shooting = false;
-            if (enemies[i][j].hits == 3) {
+            if (enemies[i][j].hits >= 3) {
               enemies[i][j].status = false;
               enemies[i][j].moving = false;
+              enemies[i][j].shooter = false;
+            }
+          } else if (
+            (bullets[q][p].x > enemies[i][j].x &&
+              bullets[q][p].x < enemies[i][j].x + circularEnemyRadius &&
+              bullets[q][p].y > enemies[i][j].y &&
+              bullets[q][p].y < enemies[i][j].y + circularEnemyRadius &&
+              bullets[q][p].shot == true &&
+              enemies[i][j].status == true &&
+              enemies[i][j].type == "circular") ||
+            (bullets[q][p].x < enemies[i][j].x &&
+              bullets[q][p].x > enemies[i][j].x - circularEnemyRadius &&
+              bullets[q][p].y < enemies[i][j].y &&
+              bullets[q][p].y > enemies[i][j].y - circularEnemyRadius &&
+              bullets[q][p].shot == true &&
+              enemies[i][j].status == true &&
+              enemies[i][j].type == "circular")
+          ) {
+            if (enemies[i][j].armour > 0) {
+              enemies[i][j].armour--;
+            } else {
+              enemies[i][j].hits++;
+            }
+            bullets[q][p].shot = false;
+            bullets[q][p].shooting = false;
+            if (enemies[i][j].hits >= 3) {
+              enemies[i][j].status = false;
+              enemies[i][j].moving = false;
+              enemies[i][j].shooter = false;
             }
           }
         }
       }
     }
   }
+  //DETECT PLAYER COLLISION
+  for (i = 0; i < 20; i++) {
+    for (j = 0; j < 5; j++) {
+      let e = enemies[i][j];
+      if (
+        x > e.x &&
+        x < e.x + rectangleEnemyWidth &&
+        y > e.y &&
+        y < e.y + rectangleEnemyLength &&
+        e.status == true &&
+        e.type == "rectangular"
+      ) {
+        if (e.hits == 0) {
+          health -= 100;
+        } else if (e.hits == 1) {
+          health -= 50;
+        } else {
+          health -= 25;
+        }
+        e.status = false;
+        e.moving = false;
+      } else if (e.type == "circular" && e.status == true) {
+        let base = Math.abs(e.x - x);
+        let height = Math.abs(e.y - y);
+        let dist = Math.sqrt(base ** 2 + height ** 2);
+        if (dist < ballRadius + circularEnemyRadius) {
+          if (e.hits == 0) {
+            health -= 100;
+          } else if (e.hits == 1) {
+            health -= 50;
+          } else {
+            health -= 25;
+          }
+          e.status = false;
+          e.moving = false;
+        }
+      } else if (e.type == "shooterbullet" && e.status == true) {
+        let base = Math.abs(e.x - x);
+        let height = Math.abs(e.y - y);
+        let dist = Math.sqrt(base ** 2 + height ** 2);
+        if (dist < ballRadius + enemybulletradius) {
+          health -= 10;
+          e.status = false;
+          e.moving = false;
+        }
+      }
+    }
+  }
 }
 
-//draw player
+//draw canvas
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawPlayer();
   createBullet();
   setEnemies();
+  bulletEnemies();
   createEnemies();
   detectCollisions();
+  drawHealth();
+  if (health <= 0) {
+    location.reload(true);
+  }
+  if (lastDifficultyScale >= Date.now() - difficultygap) {
+    rectangledelay = rectangledelay;
+  } else {
+    if (rectangledelay <= 600) {
+      rectangledelay = rectangledelay;
+    } else {
+      rectangledelay -= 10;
+      if (enemydifficultyScale < 5) {
+        enemydifficultyScale++;
+      } else {
+        enemydifficultyScale = 5;
+      }
+      lastDifficultyScale = Date.now();
+      console.log("faster");
+    }
+  }
   x += dx;
   y += dy;
   dx = 0;
   dy = 0;
   if (rightPressed == true) {
-    dx = 3;
+    if (x + ballRadius > canvas.width) {
+      dy = 0;
+    } else {
+      dx = 3;
+    }
   }
   if (leftPressed == true) {
-    dx = -3;
+    if (x - ballRadius < 0) {
+      dy = 0;
+    } else {
+      dx = -3;
+    }
   }
   if (upPressed == true) {
-    dy = -3;
+    if (y - ballRadius < 0) {
+      dy = 0;
+    } else {
+      dy = -3;
+    }
   }
   if (downPressed == true) {
-    dy = 3;
+    if (y + ballRadius > canvas.height) {
+      dy = 0;
+    } else {
+      dy = 3;
+    }
   }
   if (shooting == true) {
     let found = false;
@@ -215,14 +554,7 @@ function keyDown(e) {
     upPressed = true;
   } else if (e.keyCode == 83) {
     downPressed = true;
-  } //else if (e.key == " " || e.keycode == spacePressed) {
-  //if (lastShot >= Date.now() - delay) {
-  //shooting = false;
-  //} else {
-  //lastShot = Date.now();
-  //shooting = true;
-  //}
-  //}
+  }
 }
 
 function keyUp(e) {
@@ -234,11 +566,10 @@ function keyUp(e) {
     upPressed = false;
   } else if (e.keyCode == 83) {
     downPressed = false;
-  } //else if (e.key == " " || e.keyCode == spacePressed) {
-  //shooting = false;
-  //}
+  }
 }
 
+//SHOOT ON MOUSE CLICK
 function Shoot() {
   if (lastShot >= Date.now() - delay) {
     shooting = false;
